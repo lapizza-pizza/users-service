eaea1ec78e34745d24724dfd5243d9c98ef0cf8c
balabaev00 <balabaev00@yandex.ru>
2023-01-14 23:58:22 +0900

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 import { envConfig as env } from './config/env.config';
 import { AppModule } from './modules/server/app.module';
@@ -6,7 +8,17 @@ import { AppModule } from './modules/server/app.module';
 require('module-alias/register')
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT || 3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors();
+
+	const config = new DocumentBuilder()
+		.setTitle('User api')
+		.setDescription('user service')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(env.PORT || 3000);
 }
 bootstrap();
