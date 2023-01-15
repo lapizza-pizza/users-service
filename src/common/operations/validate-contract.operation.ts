9c686c35d2a0c6cd1ac3e4633ec34f31b0d82b5c
balabaev00 <balabaev00@yandex.ru>
2023-01-15 23:09:15 +0900

    fix: validate contract operation fix

--- src/common/operations/validate-contract.operation.ts
+++ src/common/operations/validate-contract.operation.ts
@@ -1,17 +1,15 @@
+import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 
 import { ApplicationContract } from '../contracts/application.contract';
 
-export async function validateContract<T extends ApplicationContract>(contractClass: { new(): T }, input: T): Promise<T> {
-	console.log(typeof input)
-	if (input instanceof contractClass) {
-		const errors = await validate(input, { whitelist: true, validationError: { target: false, value: false } })
+export async function validateContract<T extends ApplicationContract>(contractClass: { new(): T }, input: object): Promise<T> {
+	const contract = input instanceof contractClass ? input : plainToInstance(contractClass, input || {}, { excludeExtraneousValues: true })
+	const errors = await validate(contract, { whitelist: true, validationError: { target: false, value: false } })
 
-		if (errors.length > 0) {
-			throw new Error('validation error')
-		} else {
-			return input
-		}
+	if (errors.length > 0) {
+		throw new Error('validation error')
+	} else {
+		return contract
 	}
-	throw new Error('instance of contract class error')
 }
--- src/modules/user/commands/create-user/create-user.contract.ts
+++ src/modules/user/commands/create-user/create-user.contract.ts
@@ -1,17 +1,21 @@
 import { ApplicationContract } from '@src/common/contracts/application.contract';
 import { UUID } from '@src/common/types/uuid.brand';
+import { Expose } from 'class-transformer';
 import { IsDefined, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateUserContract extends ApplicationContract {
 	@IsUUID()
 	@IsDefined()
+	@Expose()
 	id: UUID;
 
 	@IsString()
 	@IsDefined()
+	@Expose()
 	firstName: string;
 
 	@IsString()
 	@IsOptional()
+	@Expose()
 	lastName?: string;
 }
