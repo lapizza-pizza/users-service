48c65da25187cab80b2dd332aef88c204ab49a4b
balabaev00 <balabaev00@yandex.ru>
2023-01-17 23:57:20 +0900

    feat: add get one method for user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger/dist';
+import { UUID } from '@src/common/types/uuid.brand';
 import { EntityManager } from 'typeorm';
 
 import { CreateUserCommand } from './commands/create-user/create-user.command';
@@ -26,5 +27,11 @@ export class UserController {
 	@ApiResponse({ status: 200, type: [UserEntity] })
 	async find(): Promise<UserEntity[]> {
 		return this.em.find(UserEntity)
+	}
+
+	@Get(':id')
+	@ApiResponse({ status: 200, type: UserEntity })
+	async findOne(@Param('id') id: string): Promise<UserEntity> {
+		return this.em.findOne(UserEntity, { where: { id: id as UUID, deletedAt: null } })
 	}
 }
